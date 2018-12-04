9ebd1103012a47e9294cb1589eca514c90ea1f6b
jc468597 <kphoothawnaing@my.jcu.edu.au>
2018-12-04 14:56:55 +0800

    programming

--- pratical5/do_scratch.py
+++ pratical5/do_scratch.py
@@ -1,1 +1,14 @@
-text=input("Text")
+word_count={}
+texts=input("Text: ").split()
+
+for text in texts:
+    try:
+        word_count[text]+=1
+    except KeyError:
+        word_count[text]=1
+
+for key,value in sorted(word_count.items()):
+    print(key,":",value)
+
+
+

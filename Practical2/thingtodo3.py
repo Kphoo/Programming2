6ca3591d2e461b34dc2aeccb32706a6559a2b877
K PHOO <kphoothawnaing@my.jcu.edu.au>
2019-01-17 01:50:27 +0800

    commit

---
+++ Practical2/thingtodo3.py
@@ -0,0 +1,35 @@
+
+
+import random
+
+MAX_INCREASE = 0.175  # 10%
+MAX_DECREASE = 0.05  # 5%
+MIN_PRICE = 1
+MAX_PRICE = 100.0
+INITIAL_PRICE = 10.0
+Counter=[]
+price = INITIAL_PRICE
+print("Starting price: ${:,.2f}".format(price))
+
+while price >= MIN_PRICE and price <= MAX_PRICE:
+    price_change = 0
+    # generate a random integer of 1 or 2
+    # if it's 1, the price increases, otherwise it decreases
+    if random.randint(1, 2) == 1:
+        # generate a random floating-point number
+        # between 0 and MAX_INCREASE
+        price_change = random.uniform(0, MAX_INCREASE)
+    else:
+        # generate a random floating-point number
+        # between negative MAX_DECREASE and 0
+        price_change = random.uniform(-MAX_DECREASE, 0)
+
+    price *= (1 + price_change)
+    Counter.append(price)
+
+for i in range (len(Counter)):
+    print("On day{0} price is ${1:.2f}".format(i+1,Counter[i]))
+
+out_file=open("OUTPUT_FILE.txt",'w')
+print("${:,.2f}".format(price),file=out_file)
+out_file.close()
---
+++ Practical6/ex2.py
@@ -0,0 +1,5 @@
+from Practical6.date import Date
+max = Date(21, 1, 2018)
+print(max)
+max.add_days(30)
+print(max)
